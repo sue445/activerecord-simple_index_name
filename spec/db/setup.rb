65cf66726b2744f1759537ac47fbd26829b31ce3
sue445 <sue445@sue445.net>
2019-01-20 23:05:37 +0900

    Fixed. test is failed when rails 6.0.0.beta1
    https://travis-ci.org/sue445/activerecord-simple_index_name/jobs/482038092
    
    ```
    An error occurred while loading spec_helper.
    Failure/Error:
      ActiveRecord::Base.configurations["test"] = {
        adapter:  "sqlite3",
        database: ":memory:",
        timeout:  500
      }.with_indifferent_access
    NoMethodError:
      undefined method `[]=' for #<ActiveRecord::DatabaseConfigurations:0x00000000043dcdc8>
      Did you mean?  []
    ```

--- spec/db/setup.rb
+++ spec/db/setup.rb
@@ -1,7 +1,9 @@
-ActiveRecord::Base.configurations["test"] = {
-  adapter:  "sqlite3",
-  database: ":memory:",
-  timeout:  500
+ActiveRecord::Base.configurations = {
+  test: {
+    adapter:  "sqlite3",
+    database: ":memory:",
+    timeout:  500
+  }
 }.with_indifferent_access
 
 ActiveRecord::Base.establish_connection(:test)
